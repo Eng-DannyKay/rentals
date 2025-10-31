905a78a5df10b4f475b44703227685b45b83bfea
Daniel Forson <danielkojoforson27@gmail.com>
2025-10-31 17:26:47 +0000

    refactor: adjust the width and height of the logo

--- components/authenticationFrame.tsx
+++ components/authenticationFrame.tsx
@@ -1,4 +1,4 @@
-import { DarkLogo } from "@/assets/imageIndex";
+import { Logo } from "@/assets/imageIndex";
 import { Poppins } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
@@ -26,7 +26,7 @@ const AuthenticationFrame = ({
       <div className="bg-white max-w-[550px] max-h-[100vh] w-full  m-auto p-6 shadow-lg rounded-[8px] text-[#23292E] ">
         <div className="mx-auto flex items-center justify-center flex-col gap-2 mb-6">
           <Link href="/">
-            <Image src={DarkLogo} alt="dark logo" />
+            <Image src={Logo} width={12} height={12} alt="dark logo" />
           </Link>
           <p className="font-bold text-2xl leading-8">{title}</p>
           <p className="text-[#45535F] leading-[130%]">{subText}</p>
